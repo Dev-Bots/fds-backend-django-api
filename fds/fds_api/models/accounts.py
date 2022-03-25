c03df8dacb8d511d7b35b86f48f791b1f4fb4664
Meron Mesay <it.meron.mesay@gmail.com>
2022-03-25 14:13:24 +0300

    created Account class

--- fds/fds_api/models/accounts.py
+++ fds/fds_api/models/accounts.py
@@ -9,3 +9,34 @@ PROFILE_PICTURE_PATH = 'assests/images/profile_pictures/'
 BIRTH_CERTIFICATE_PATH = 'assets/images/birth_certifcate/'
 EDUCATION_CERTIFICATE_PATH = 'assets/images/education/'
 VIDEO_PATH = 'assets/video/'
+
+class Account(AbstractUser):
+    class Types(models.TextChoices):
+        PLAYER = "PLAYER", "Player"
+        SCOUT = "SCOUT", "Scout"
+        CLUB = "CLUB", "Club"
+        ADMIN = "ADMIN"
+
+
+    base_type = Types.ADMIN
+    is_active = models.BooleanField(default=True)
+    type = models.CharField(
+        _("Type"), max_length=50, choices=Types.choices, default=base_type
+    )
+
+    phone_number = models.CharField(_("Phone number"), blank=False, max_length=255)
+    email = models.EmailField(_("Email"), blank=False, max_length=255)
+    profile_picture =  models.ImageField(_("Profile picture"), upload_to=PROFILE_PICTURE_PATH, blank=True)
+    address = models.CharField(_("Address"), max_length=255, blank=False)
+    
+    
+
+    USERNAME_FIELD = 'username'
+
+    def get_absolute_url(self):
+            return reverse("accounts:detail", kwargs={"username": self.username})
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.type = self.base_type
+        return super().save(*args, **kwargs)
