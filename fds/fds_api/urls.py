805eb1d5b1c970874c20c02a25bb33db6a57a084
kevin-shitaye <it.kevin.shitaye@gmail.com>
2022-03-27 20:13:34 +0300

    created the event model

--- fds/fds_api/urls.py
+++ fds/fds_api/urls.py
@@ -1,22 +1,22 @@
 
 from django.urls import path, include
-from .views import Clubs, Players, Scouts, Events, EventActions, SkillView, ParameterView, GradeView, Results
+# from .views import Clubs, Players, Scouts, Events, EventActions, SkillView, ParameterView, GradeView, Results
 from rest_framework.routers import DefaultRouter
 
 app_name = 'fds_api'
 
 router = DefaultRouter()
-router.register(r'players', Players, basename="players")
-router.register(r'clubs', Clubs, basename="clubs")
-router.register(r'scouts', Scouts, basename="scouts")
+# router.register(r'players', Players, basename="players")
+# router.register(r'clubs', Clubs, basename="clubs")
+# router.register(r'scouts', Scouts, basename="scouts")
 
-router.register(r'events', Events, basename='events')
-router.register(r'event_actions', EventActions, basename='eventactions')
+# router.register(r'events', Events, basename='events')
+# router.register(r'event_actions', EventActions, basename='eventactions')
 
-router.register(r'skills', SkillView, basename='skill')
-router.register(r'parameters', ParameterView, basename='parameters')
-router.register(r'grade', GradeView, basename='grade' )
-router.register(r'results', Results, basename='results')
+# router.register(r'skills', SkillView, basename='skill')
+# router.register(r'parameters', ParameterView, basename='parameters')
+# router.register(r'grade', GradeView, basename='grade' )
+# router.register(r'results', Results, basename='results')
 
 urlpatterns = [
     path('', include(router.urls)),
