f65a11383a742dc7322b1b07c9af766e420db3b1
kevin-shitaye <it.kevin.shitaye@gmail.com>
2022-03-30 22:44:35 +0300

    made applying for event endpoint

--- fds/fds_api/views/event_views.py
+++ fds/fds_api/views/event_views.py
@@ -10,3 +10,13 @@ class Events(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
     
+class EventActions(viewsets.ViewSet):
+
+    #api/event_actions/event_id/apply   !!!!!!permission needed desperately!!!!!!!!!!!!!
+    @action(methods=['post'], detail=True)
+    def apply(self, request, pk=None):
+        event = get_object_or_404(Event, id=pk)
+        player = get_object_or_404(Player, id=request.user.id)
+        event.applicants.add(player)
+        event.save()
+        return Response({"Message": "Succesfully applied the event."}, status=status.HTTP_202_ACCEPTED)
