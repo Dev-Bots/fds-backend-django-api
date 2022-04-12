cc946018de81fe927afe3ac6888c72400afe6ef5
Meron Mesay <it.meron.mesay@gmail.com>
2022-04-12 08:26:33 +0300

    player and club class created

--- fds/fds_api/views/account_views.py
+++ fds/fds_api/views/account_views.py
@@ -4,3 +4,15 @@ from rest_framework import generics, permissions, viewsets
 from ..permissions import *
 from ..models import Player, Club, Scout
 from ..serializers import PlayerSerializer, ClubSerializer, ScoutSerializer
+
+# Create your views here.
+
+class Players(viewsets.ModelViewSet):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    
+   
+class Clubs(viewsets.ModelViewSet):
+    queryset = Club.objects.all()
+    serializer_class = ClubSerializer
