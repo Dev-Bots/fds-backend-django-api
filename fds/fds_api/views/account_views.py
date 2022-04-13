a30f8c757bfca3c506a924286f6ac1989f9272c0
Meron Mesay <it.meron.mesay@gmail.com>
2022-04-13 13:29:55 +0300

    modified viewset actions

--- fds/fds_api/views/account_views.py
+++ fds/fds_api/views/account_views.py
@@ -23,3 +23,12 @@ class Scouts(viewsets.ModelViewSet):
     serializer_class = ScoutSerializer   
     permission_classes = [IsClubOrScoutEditRetrive]
 
+""" VIEWSET ACTIONS
+
+    def list(self, request):
+        pass
+
+    def create(self, request):
+        pass
+
+"""
