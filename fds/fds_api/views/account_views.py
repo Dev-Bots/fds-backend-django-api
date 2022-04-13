b856299a3e42f533855153224839fa944b8cbaf7
Meron Mesay <it.meron.mesay@gmail.com>
2022-04-13 13:31:20 +0300

    more changes made on viewset action

--- fds/fds_api/views/account_views.py
+++ fds/fds_api/views/account_views.py
@@ -31,4 +31,16 @@ class Scouts(viewsets.ModelViewSet):
     def create(self, request):
         pass
 
+    def retrieve(self, request, pk=None):
+        pass
+
+    def update(self, request, pk=None):
+        pass
+
+    def partial_update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        pass
 """
+
